774ed6b982c38a346956375e8e39dc7607d602de
MateuszW <mwiczuk99@gmail.com>
2021-06-01 22:37:59 +0200

    Fix errors in startup

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -18,7 +18,8 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddControllers();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,11 +42,9 @@ namespace Api
 
             app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
