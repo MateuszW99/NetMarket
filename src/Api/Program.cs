5a5995507030a827f186e09a0f1002e3e9dfaf73
MateuszW <mwiczuk99@gmail.com>
2021-06-10 14:09:32 +0200

    Fix database creation on startup

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,13 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                
+                await context.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                // TODO: add logs
+            }
+            
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
