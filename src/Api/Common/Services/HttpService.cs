eb534bb27966a6a572abaca91dc938475e51faea
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-06-14 20:29:45 +0200

    Add IHttpService

---
+++ src/Api/Common/Services/HttpService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Claims;
+using Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Api.Common.Services
+{
+    public class HttpService : IHttpService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HttpService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserId()
+        {
+            if (_httpContextAccessor.HttpContext?.User == null)
+            {
+                return String.Empty;
+            }
+
+            return _httpContextAccessor.HttpContext.User.FindFirstValue("id") ?? string.Empty;
+        }
+    }
+}
