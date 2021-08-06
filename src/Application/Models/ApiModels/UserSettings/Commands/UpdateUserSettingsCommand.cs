893f4dbd7dc579bca1c72928296e487b7ad56b7a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-08-06 14:17:56 +0200

    Add UpdateUserSettingsCommand validator

--- src/Application/Models/ApiModels/UserSettings/Commands/UpdateUserSettingsCommand.cs
+++ src/Application/Models/ApiModels/UserSettings/Commands/UpdateUserSettingsCommand.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Enums;
+﻿using Application.Common.Validators;
+using Domain.Enums;
+using FluentValidation;
 using MediatR;
 
 namespace Application.Models.ApiModels.UserSettings.Commands
@@ -8,17 +10,39 @@ namespace Application.Models.ApiModels.UserSettings.Commands
         public SellerLevel SellerLevel { get; set; }
         public int SalesCompleted { get; set; }
         public string PaypalEmail { get; set; }
-        
+
         public string BillingStreet { get; set; }
         public string BillingAddressLine1 { get; set; }
         public string BillingAddressLine2 { get; set; }
         public string BillingZipCode { get; set; }
         public string BillingCountry { get; set; }
-        
+
         public string ShippingStreet { get; set; }
         public string ShippingAddressLine1 { get; set; }
         public string ShippingAddressLine2 { get; set; }
         public string ShippingZipCode { get; set; }
         public string ShippingCountry { get; set; }
+    }
+
+    public class UpdateUserSettingsCommandValidator : AbstractValidator<UpdateUserSettingsCommand>
+    {
+        public UpdateUserSettingsCommandValidator()
+        {
+            RuleFor(x => x.SellerLevel).IsInEnum();
+            RuleFor(x => x.SalesCompleted).GreaterThanOrEqualTo(0).LessThan(int.MaxValue);
+            RuleFor(x => x.PaypalEmail).EmailAddress().MaximumLength(40);
+
+            RuleFor(x => x.BillingStreet).MaximumLength(50);
+            RuleFor(x => x.BillingAddressLine1).MaximumLength(50);
+            RuleFor(x => x.BillingAddressLine2).MaximumLength(50);
+            RuleFor(x => x.BillingZipCode).MustMatchZipCodePattern().MaximumLength(6);
+            RuleFor(x => x.BillingCountry).MaximumLength(50);
+
+            RuleFor(x => x.ShippingStreet).MaximumLength(50);
+            RuleFor(x => x.ShippingAddressLine1).MaximumLength(50);
+            RuleFor(x => x.ShippingAddressLine2).MaximumLength(50);
+            RuleFor(x => x.ShippingZipCode).MustMatchZipCodePattern().MaximumLength(6);
+            RuleFor(x => x.ShippingCountry).MaximumLength(50);
+        }
     }
 }
