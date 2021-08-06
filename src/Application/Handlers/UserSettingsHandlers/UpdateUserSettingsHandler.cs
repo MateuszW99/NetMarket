c58f6eb903a69d5a3369b882549d010a056e34fc
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-08-06 18:54:05 +0200

    Add UpdateUserSettingsHandler

---
+++ src/Application/Handlers/UserSettingsHandlers/UpdateUserSettingsHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Models.ApiModels.UserSettings.Commands;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Handlers.UserSettingsHandlers
+{
+    public class UpdateUserSettingsHandler : IRequestHandler<UpdateUserSettingsCommand>
+    {
+        private readonly IUserSettingsService _userSettingsService;
+        private readonly IHttpService _httpService;
+        private readonly ILogger<GetUserSettingsHandler> _logger;
+
+        public UpdateUserSettingsHandler(IUserSettingsService userSettingsService, IHttpService httpService,
+            ILogger<GetUserSettingsHandler> logger)
+        {
+            _userSettingsService = userSettingsService;
+            _httpService = httpService;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateUserSettingsCommand request, CancellationToken cancellationToken)
+        {
+            var currentUserId = Guid.Parse(_httpService.GetUserId());
+
+            await _userSettingsService.UpdateUserSettingsAsync(currentUserId, request, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
