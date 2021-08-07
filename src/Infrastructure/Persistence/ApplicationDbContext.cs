69b7bcb7d13adcf598e8706128425a2876d594ce
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-08-07 10:59:57 +0200

    Add setting BaseEntity properties in SaveChangesAsync method

--- src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -13,17 +13,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence
 {
-    public class ApplicationDbContext 
+    public class ApplicationDbContext
         : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>, IApplicationDbContext
     {
         private readonly IDomainEventService _domainEventService;
-        
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IDomainEventService domainEventService) 
+        private readonly IHttpService _httpService;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
+            IDomainEventService domainEventService, IHttpService httpService)
             : base(options)
         {
             _domainEventService = domainEventService;
+            _httpService = httpService;
         }
-        
+
         public DbSet<Ask> Asks { get; set; }
         public DbSet<Bid> Bids { get; set; }
         public DbSet<Brand> Brands { get; set; }
@@ -32,15 +35,31 @@ namespace Infrastructure.Persistence
         public DbSet<Size> Sizes { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<UserSettings> UserSettings { get; set; }
-        
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(builder);
         }
-        
+
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
         {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = Guid.Parse(_httpService.GetUserId());
+                        entry.Entity.Created = DateTime.Now;
+                        break;
+
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = Guid.Parse(_httpService.GetUserId());
+                        entry.Entity.LastModified = DateTime.Now;
+                        break;
+                }
+            }
+
             var result = await base.SaveChangesAsync(cancellationToken);
             await DispatchEvents();
             return result;
