d4c524d3f7e3e2ee664ca0d2026583e072bffe61
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-08-09 22:36:38 +0200

    Feature/user-settings-service (#11)
    * Add UpdateUserSettingsCommand and GetUserSettingsQuery
    
    * Add UpdateUserSettingsCommand validator
    
    * Add NotFoundException
    
    * Add UserSettingsService
    
    * Add GetUserSettingsHandler
    
    * Add UpdateUserSettingsHandler
    
    * Add UserController, fixes in UpdateUserSettings method
    
    * Add setting BaseEntity properties in SaveChangesAsync method
    
    * Add ZipCode validation tests
    
    * Fix logger initialization in UpdateUserSettingsHandler
    
    * Add UserSettingsCommandHandlersTests
    
    * Change UpdateUserSettings response from NoContent to Ok
    
    * Change ZipCode max length in UserSettings entity configuration
    
    * Remove logger from user settings handlers
    
    * Fix Dockerfile
    
    * Remove SellerLevel and SalesCompleted from UpdateUserSettingsCommand
    
    * Change GetUserSettingsAsync method so that it always returns a UserSettings object
    
    * Add CreateUserSettingsAsync method to UserSettingsService
    
    * Add Application.IntegrationTests project
    
    * Rename classes
    
    * Change if statement in UpdateUserSettingsAsync method
    
    * Setup integration tests by adding CustomWebApplicationFactory and abstract IntegrationTest class
    
    * Add AuthHelper
    
    * Add integration tests for GetUserSettings
    
    * Add integration tests for UpdateUserSettings

---
+++ tests/Application.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using Api;
+using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Application.IntegrationTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        public CustomWebApplicationFactory()
+        {
+            _databaseName = Guid.NewGuid().ToString();
+        }
+
+        private readonly string _databaseName;
+
+        public string CurrentUserId { get; set; }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                //Replace HttpService
+                var httpServiceDescriptor = services.FirstOrDefault(d =>
+                    d.ServiceType == typeof(IHttpService));
+
+                services.Remove(httpServiceDescriptor);
+                services.AddTransient(_ =>
+                    Mock.Of<IHttpService>(x => x.GetUserId() == CurrentUserId));
+
+
+                //Replace ApplicationDbContext
+                var applicationDbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<ApplicationDbContext>));
+
+                services.Remove(applicationDbContextDescriptor);
+                services.AddDbContext<ApplicationDbContext>(options => { options.UseInMemoryDatabase(_databaseName); });
+                
+                var sp = services.BuildServiceProvider();
+
+                using var scope = sp.CreateScope();
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var context = scopedServices.GetRequiredService<ApplicationDbContext>();
+                    var roleSeeder = scopedServices.GetRequiredService<RoleSeeder>();
+
+                    var userSeeder = scopedServices.GetRequiredService<UserSeeder>();
+                    var logger = scopedServices
+                        .GetRequiredService<ILogger<CustomWebApplicationFactory>>();
+
+                    context.Database.EnsureCreatedAsync().Wait();
+
+                    try
+                    {
+                        roleSeeder.SeedAsync().Wait();
+                        userSeeder.SeedAsync().Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the test database" +
+                                            "Error: {Message}", ex.Message);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ tests/Application.IntegrationTests/UserSettings/Commands/UpdateUserSettingsTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.IntegrationTests.Helpers;
+using Application.Models.ApiModels.UserSettings.Commands;
+using Domain.Enums;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Application.IntegrationTests.UserSettings.Commands
+{
+    public class UpdateUserSettingsTests : IntegrationTest
+    {
+        [Fact]
+        public async Task ShouldCreateUserSettingsIfNotCreated()
+        {
+            var userId = await AuthHelper.RunAsDefaultUserAsync(_factory);
+            var command = new UpdateUserSettingsCommand()
+            {
+                PaypalEmail = "test@test.com",
+
+                BillingStreet = "Test billing street",
+                BillingAddressLine1 = "Test billing address line 1",
+                BillingAddressLine2 = "Test billing address line 2",
+                BillingZipCode = "12-345",
+                BillingCountry = "USA",
+
+                ShippingStreet = "Test shipping street",
+                ShippingAddressLine1 = "Test shipping address line 1",
+                ShippingAddressLine2 = "Test shipping address line 2",
+                ShippingZipCode = "12-345",
+                ShippingCountry = "USA"
+            };
+
+            await _mediator.Send(command);
+
+            //assert that user settings have been added to the database
+            var context = DbHelper.GetDbContext(_factory);
+            var userSettings = await context.UserSettings.FirstOrDefaultAsync(x => x.UserId == Guid.Parse(userId));
+
+            userSettings.Id.Should().NotBeEmpty();
+            userSettings.UserId.Should().Be(userId);
+            userSettings.SellerLevel.Should().Be(SellerLevel.Beginner);
+            userSettings.SalesCompleted.Should().Be(0);
+            userSettings.PaypalEmail.Should().Be(command.PaypalEmail);
+            userSettings.BillingStreet.Should().Be(command.BillingStreet);
+            userSettings.BillingAddressLine1.Should().Be(command.BillingAddressLine1);
+            userSettings.BillingAddressLine2.Should().Be(command.BillingAddressLine2);
+            userSettings.BillingZipCode.Should().Be(command.BillingZipCode);
+            userSettings.BillingCountry.Should().Be(command.BillingCountry);
+            userSettings.ShippingStreet.Should().Be(command.ShippingStreet);
+            userSettings.ShippingAddressLine1.Should().Be(command.ShippingAddressLine1);
+            userSettings.ShippingAddressLine2.Should().Be(command.ShippingAddressLine2);
+            userSettings.ShippingZipCode.Should().Be(command.ShippingZipCode);
+            userSettings.ShippingCountry.Should().Be(command.ShippingCountry);
+
+            userSettings.CreatedBy.Should().Be(userId);
+            userSettings.Created.Should().BeCloseTo(DateTime.Now, 1000);
+            userSettings.LastModified.Should().BeNull();
+            userSettings.LastModifiedBy.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task ShouldUpdateUserSettingsIfCreated()
+        {
+            var userId = await AuthHelper.RunAsDefaultUserAsync(_factory);
+            
+            //First, create settings
+            var command = new UpdateUserSettingsCommand()
+            {
+                PaypalEmail = "test@test.com",
+
+                BillingStreet = "Test billing street",
+                BillingAddressLine1 = "Test billing address line 1",
+                BillingAddressLine2 = "Test billing address line 2",
+                BillingZipCode = "12-345",
+                BillingCountry = "USA",
+
+                ShippingStreet = "Test shipping street",
+                ShippingAddressLine1 = "Test shipping address line 1",
+                ShippingAddressLine2 = "Test shipping address line 2",
+                ShippingZipCode = "12-345",
+                ShippingCountry = "USA"
+            };
+
+            await _mediator.Send(command);
+            
+            //Update settings
+            command = new UpdateUserSettingsCommand()
+            {
+                PaypalEmail = "test@test.com",
+
+                BillingStreet = "Updated billing street",
+                BillingAddressLine1 = "Updated billing address line 1",
+                BillingAddressLine2 = "Updated billing address line 2",
+                BillingZipCode = "12-345",
+                BillingCountry = "USA",
+
+                ShippingStreet = "Updated shipping street",
+                ShippingAddressLine1 = "Updated shipping address line 1",
+                ShippingAddressLine2 = "Updated shipping address line 2",
+                ShippingZipCode = "12-345",
+                ShippingCountry = "USA"
+            };
+            
+            await _mediator.Send(command);
+
+            //assert that updated user settings have been added to the database
+            var context = DbHelper.GetDbContext(_factory);
+            var userSettings = await context.UserSettings.FirstOrDefaultAsync(x => x.UserId == Guid.Parse(userId));
+
+            userSettings.Id.Should().NotBeEmpty();
+            userSettings.UserId.Should().Be(userId);
+            userSettings.SellerLevel.Should().Be(SellerLevel.Beginner);
+            userSettings.SalesCompleted.Should().Be(0);
+            userSettings.PaypalEmail.Should().Be(command.PaypalEmail);
+            userSettings.BillingStreet.Should().Be(command.BillingStreet);
+            userSettings.BillingAddressLine1.Should().Be(command.BillingAddressLine1);
+            userSettings.BillingAddressLine2.Should().Be(command.BillingAddressLine2);
+            userSettings.BillingZipCode.Should().Be(command.BillingZipCode);
+            userSettings.BillingCountry.Should().Be(command.BillingCountry);
+            userSettings.ShippingStreet.Should().Be(command.ShippingStreet);
+            userSettings.ShippingAddressLine1.Should().Be(command.ShippingAddressLine1);
+            userSettings.ShippingAddressLine2.Should().Be(command.ShippingAddressLine2);
+            userSettings.ShippingZipCode.Should().Be(command.ShippingZipCode);
+            userSettings.ShippingCountry.Should().Be(command.ShippingCountry);
+
+            userSettings.CreatedBy.Should().Be(userId);
+            userSettings.LastModified.Should().BeCloseTo(DateTime.Now, 1000);
+            userSettings.LastModifiedBy.Should().Be(userId);
+        }
+    }
+}
