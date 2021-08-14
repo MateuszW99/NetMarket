cebcda4ec6ef15e047cf18190afc5a1d53d31a28
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-08-14 15:48:02 +0200

    Add more assertions to the tests

--- tests/Application.IntegrationTests/Items/Commands/CreateItemTests.cs
+++ tests/Application.IntegrationTests/Items/Commands/CreateItemTests.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 using Application.IntegrationTests.Helpers;
 using Application.Models.ApiModels.Items.Commands;
 using Application.Models.DTOs;
+using Domain.Entities;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -18,6 +20,8 @@ namespace Application.IntegrationTests.Items.Commands
         [Fact]
         public async Task AdministratorShouldAddNewItem()
         {
+            var context = DbHelper.GetDbContext(_factory);
+            
             var userId = await AuthHelper.RunAsAdministratorAsync(_factory);
             var authResult = _identityService.LoginAsync(AdminUser.Email, AdminUser.Password);
             
@@ -33,19 +37,33 @@ namespace Application.IntegrationTests.Items.Commands
                 RetailPrice = 200,
                 Description = "Jordan Brand paid homage to MJ’s alma mater with the Air Jordan 4 University Blue. The University Blue colorway draws a close resemblance to the extremely rare Air Jordan 4 UNC PE that was given to Tarheel student-athletes in 2019.\name\name\nThe Air Jordan 4 University Blue features a University Blue suede upper with mesh netting on the quarter panel and tongue. Similar to OG Jordan 4 colorways of the past, the eyelets, heel tab, and sections of the midsole are a speckled Cement Grey. Hits of black appear on the wing eyelets, sole, and Jumpman logo on the heel tab. Two woven labels are stitched to the tongue; one being the classic Jumpman woven label; the other being a Team Jordan jock tag. A black, white, and Tech Grey sole with a clear Air unit completes the design.\name\name\nThe Air Jordan 4 University Blue released in April of 2021 and retailed for $200."
             };
-            
+
             var request = new HttpRequestMessage(HttpMethod.Post, new Uri($"{ApiBaseAddress}/{ItemsAddress}", UriKind.Relative));
             request.Content = new StringContent(JsonConvert.SerializeObject(command), Encoding.UTF8, "application/json");
 
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.Result.Token);
             var response = await _client.SendAsync(request);
-            
-            response.StatusCode.Should().BeEquivalentTo(StatusCodes.Status200OK);
+
+            var newItem = await context.Items
+                .FirstOrDefaultAsync(x => x.Name == command.Name);
+
+            response.StatusCode.Should().Be(StatusCodes.Status200OK);
+            newItem.Should().NotBeNull();
+            newItem.ImageUrl.Should().Be(command.ImageUrl);
+            newItem.SmallImageUrl.Should().Be(command.SmallImageUrl);
+            newItem.ThumbUrl.Should().Be(command.ThumbUrl);
+            newItem.Make.Should().Be(command.Make);
+            newItem.Model.Should().Be(command.Model);
+            newItem.RetailPrice.Should().Be(command.RetailPrice);
+            newItem.Description.Should().Be(command.Description);
         }
 
         [Fact]
         public async Task SupervisorShouldNotAddNewItem()
         {
+            var context = DbHelper.GetDbContext(_factory);
+            var oldItemsCount = context.Items.CountAsync();
+            
             var userId = await AuthHelper.RunAsSupervisorAsync(_factory);
             var authResult = _identityService.LoginAsync(SupervisorUser.Email, SupervisorUser.Password);
             
@@ -67,13 +85,19 @@ namespace Application.IntegrationTests.Items.Commands
 
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.Result.Token);
             var response = await _client.SendAsync(request);
+
+            var newCount = await context.Items.CountAsync();
             
-            response.StatusCode.Should().BeEquivalentTo(StatusCodes.Status403Forbidden);
+            response.StatusCode.Should().Be(StatusCodes.Status403Forbidden);
+            newCount.Should().Be(await oldItemsCount);
         }
         
         [Fact]
         public async Task UserShouldNotAddNewItem()
         {
+            var context = DbHelper.GetDbContext(_factory);
+            var oldItemsCount = context.Items.CountAsync();
+            
             var userId = await AuthHelper.RunAsDefaultUserAsync(_factory);
             var authResult = _identityService.LoginAsync(DefaultUser.Email, DefaultUser.Password);
             
@@ -96,12 +120,18 @@ namespace Application.IntegrationTests.Items.Commands
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.Result.Token);
             var response = await _client.SendAsync(request);
             
-            response.StatusCode.Should().BeEquivalentTo(StatusCodes.Status403Forbidden);
+            var newCount = await context.Items.CountAsync();
+            
+            response.StatusCode.Should().Be(StatusCodes.Status403Forbidden);
+            newCount.Should().Be(await oldItemsCount);
         }
 
         [Fact]
         public async Task NotLoggedInUserShouldNotAddNewItem()
         {
+            var context = DbHelper.GetDbContext(_factory);
+            var oldItemsCount = context.Items.CountAsync();
+            
             var command = new CreateItemCommand()
             {
                 Brand = new BrandObject() {Id = Guid.NewGuid().ToString(), Name = "A brand new Brand"},
@@ -120,7 +150,10 @@ namespace Application.IntegrationTests.Items.Commands
             
             var response = await _client.SendAsync(request);
             
-            response.StatusCode.Should().BeEquivalentTo(StatusCodes.Status401Unauthorized);
+            var newCount = await context.Items.CountAsync();
+            
+            response.StatusCode.Should().Be(StatusCodes.Status401Unauthorized);
+            newCount.Should().Be(await oldItemsCount);
         }
     }
 }
