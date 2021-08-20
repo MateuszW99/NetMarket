1e57d9f69169612a59b8ad6078a6077c64e435ef
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-08-20 19:44:49 +0200

    Add GetTransactionsQuery and GetTransactionsQueryValidator

---
+++ src/Application/Models/ApiModels/Transactions/Queries/GetTransactionsQuery.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Models;
+using Application.Models.DTOs;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Models.ApiModels.Transactions.Queries
+{
+    public class GetTransactionsQuery : IRequest<TransactionObject>
+    {
+        public SearchTransactionsQuery SearchTransactionsQuery { get; set; }
+    }
+
+    public class GetTransactionsQueryValidator : AbstractValidator<GetTransactionsQuery>
+    {
+        public GetTransactionsQueryValidator()
+        {
+            RuleFor(x => x.SearchTransactionsQuery.Status).IsInEnum();
+            RuleFor(x => x.SearchTransactionsQuery.PageIndex).GreaterThanOrEqualTo(1);
+            RuleFor(x => x.SearchTransactionsQuery.PageSize).GreaterThanOrEqualTo(10);
+        }
+    }
+}
