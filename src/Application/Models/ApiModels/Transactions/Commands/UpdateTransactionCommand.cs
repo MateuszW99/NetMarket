347702c03a8a1058d5db66f96f21774a7838ea97
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-08-20 19:56:14 +0200

    Add UpdateTransactionCommand and UpdateTransactionCommandValidator

---
+++ src/Application/Models/ApiModels/Transactions/Commands/UpdateTransactionCommand.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Validators;
+using Application.Models.DTOs;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Models.ApiModels.Transactions.Commands
+{
+    public class UpdateTransactionCommand : IRequest
+    {
+        public string Id { get; set; }
+        public AskObject Ask { get; set; }
+        public BidObject Bid { get; set; }
+        public string Status { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public decimal SellerFee { get; set; }
+        public decimal BuyerFee { get; set; }
+        public decimal Payout { get; set; }
+    }
+
+    public class UpdateTransactionCommandValidator : AbstractValidator<UpdateTransactionCommand>
+    {
+        public UpdateTransactionCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotNull()
+                .IdMustMatchGuidPattern();
+
+            RuleFor(x => x.Status).NotEmpty().IsInEnum();
+            RuleFor(x => x.StartDate).NotEmpty();
+            RuleFor(x => x.EndDate).NotEmpty();
+            RuleFor(x => x.SellerFee).NotEmpty().GreaterThanOrEqualTo(0);
+            RuleFor(x => x.BuyerFee).NotEmpty().GreaterThanOrEqualTo(0);
+            RuleFor(x => x.Payout).NotEmpty().GreaterThanOrEqualTo(0);
+        }
+    }
+}
