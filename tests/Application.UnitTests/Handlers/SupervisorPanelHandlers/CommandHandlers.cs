cf44a96640e3b29a7764c749bd94d4cbea4a17f2
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-09-09 11:01:58 +0200

    Add unit tests

---
+++ tests/Application.UnitTests/Handlers/SupervisorPanelHandlers/CommandHandlers.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Behaviours;
+using Application.Handlers.TransactionHandlers;
+using Application.Models.ApiModels.Transactions.Commands;
+using FluentAssertions;
+using FluentValidation;
+using Xunit;
+
+namespace Application.UnitTests.Handlers.SupervisorPanelHandlers
+{
+    public class CommandHandlers
+    {
+        [Fact]
+        public async Task UpdateTransactionStatusHandlerShouldNotThrowWhenPropertiesAreValid()
+        {
+            var updateTransactionStatusCommand = new UpdateTransactionStatusCommand()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Status = "Arrived",
+            };
+
+            var commandHandler = new UpdateTransactionStatusCommandHandler(null, null, null);
+            var validationBehaviour = new ValidationBehaviour<UpdateTransactionStatusCommand, MediatR.Unit>(new List<UpdateTransactionStatusCommandValidator>()
+            {
+                new()
+            }, null);
+
+            await FluentActions.Invoking(() =>
+                    validationBehaviour.Handle(
+                        updateTransactionStatusCommand,
+                        CancellationToken.None, 
+                        () => commandHandler.Handle(updateTransactionStatusCommand, CancellationToken.None)))
+                .Should()
+                .NotThrowAsync<ValidationException>();
+        }
+
+        [Theory]
+        [MemberData(nameof(Transactions))]
+        public async Task UpdateTransactionStatusCommandHandlerShouldThrowWhenOneOrMorePropertiesAreInvalid(string id, string status)
+        {
+            var updateTransactionStatusCommand = new UpdateTransactionStatusCommand()
+            {
+                Id = id,
+                Status = status,
+            };
+
+            var commandHandler = new UpdateTransactionStatusCommandHandler(null, null, null);
+            var validationBehaviour = new ValidationBehaviour<UpdateTransactionStatusCommand, MediatR.Unit>(new List<UpdateTransactionStatusCommandValidator>()
+            {
+                new()
+            }, null);
+
+            await FluentActions.Invoking(() =>
+                    validationBehaviour.Handle(
+                        updateTransactionStatusCommand,
+                        CancellationToken.None, 
+                        () => commandHandler.Handle(updateTransactionStatusCommand, CancellationToken.None)))
+                .Should()
+                .ThrowAsync<ValidationException>();
+        }
+        
+        public static IEnumerable<object[]> Transactions => new List<object[]>
+        {
+            new object[]
+            {
+                "", "Delivered"
+            },
+            new object[]
+            {
+                "123", "Checked"
+            },
+            new object[]
+            {
+                "", "Delivered"
+            },
+            new object[]
+            {
+                Guid.NewGuid().ToString(), "test"
+            },
+        };
+    }
+}
---
+++ tests/Application.UnitTests/Handlers/SupervisorPanelHandlers/QueryHandlers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Behaviours;
+using Application.Handlers.TransactionHandlers;
+using Application.Models.ApiModels.Transactions.Queries;
+using Application.Models.DTOs;
+using FluentAssertions;
+using FluentValidation;
+using Xunit;
+
+namespace Application.UnitTests.Handlers.SupervisorPanelHandlers
+{
+    public class QueryHandlers
+    {
+        [Fact]
+        public async Task GetTransactionByIdQueryShouldNotThrownWhenIdIsValid()
+        {
+            var query = new GetTransactionByIdQuery()
+            {
+                Id = Guid.NewGuid().ToString()
+            };
+
+            var queryHandler = new GetTransactionByIdQueryHandler(null, null, null);
+            var validationBehaviour = new ValidationBehaviour<GetTransactionByIdQuery, TransactionObject>(
+                new List<GetTransactionByIdQueryValidator>
+                {
+                    new()
+                }, null);
+
+            await FluentActions.Invoking(() => validationBehaviour.Handle(
+                    query,
+                    CancellationToken.None,
+                    () => queryHandler.Handle(query, CancellationToken.None)))
+                .Should()
+                .NotThrowAsync<ValidationException>();
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData("1")]
+        [InlineData("123-456-789-421")]
+        public async Task GetTransactionByIdQueryShouldThrownWhenIdIsInvalid(string id)
+        {
+            var query = new GetTransactionByIdQuery()
+            {
+                Id = id
+            };
+
+            var queryHandler = new GetTransactionByIdQueryHandler(null, null, null);
+            var validationBehaviour = new ValidationBehaviour<GetTransactionByIdQuery, TransactionObject>(
+                new List<GetTransactionByIdQueryValidator>
+                {
+                    new()
+                }, null);
+
+            await FluentActions.Invoking(() => validationBehaviour.Handle(
+                    query,
+                    CancellationToken.None,
+                    () => queryHandler.Handle(query, CancellationToken.None)))
+                .Should()
+                .ThrowAsync<ValidationException>();
+        }
+    }
+}
