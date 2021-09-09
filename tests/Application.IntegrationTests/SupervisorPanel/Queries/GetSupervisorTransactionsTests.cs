d7b2c95d2ec7d7c960770a9cc3a0755d41c0a644
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-09-09 20:33:07 +0200

    Add UpdateTransactionStatus integration tests

---
+++ tests/Application.IntegrationTests/SupervisorPanel/Queries/GetSupervisorTransactionsTests.cs
@@ -0,0 +1,17 @@
+﻿using Application.IntegrationTests.Helpers;
+using Application.IntegrationTests.Helpers.Deserializers;
+using Application.Models.DTOs;
+
+namespace Application.IntegrationTests.SupervisorPanel.Queries
+{
+    public class GetSupervisorTransactionsTests: IntegrationTest
+    {
+        private readonly IObjectDeserializer<TransactionObject> _transactionObjectDeserializer;
+
+        public GetSupervisorTransactionsTests()
+        {
+            _transactionObjectDeserializer = new TransactionObjectDeserializer();
+        }
+        
+    }
+}
---
+++ tests/Application.IntegrationTests/SupervisorPanel/Queries/GetTransactionByIdTests.cs
@@ -0,0 +1,17 @@
+﻿using Application.IntegrationTests.Helpers;
+using Application.IntegrationTests.Helpers.Deserializers;
+using Application.Models.DTOs;
+
+namespace Application.IntegrationTests.SupervisorPanel.Queries
+{
+    public class GetTransactionByIdTests: IntegrationTest
+    {
+        private readonly IObjectDeserializer<TransactionObject> _transactionObjectDeserializer;
+
+        public GetTransactionByIdTests()
+        {
+            _transactionObjectDeserializer = new TransactionObjectDeserializer();
+        }
+        
+    }
+}
