c08ee76d7bd69661eb4f18bde8b729c2366a0586
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-09-09 07:32:54 +0200

    Add quries and commands with validators

---
+++ src/Application/Models/ApiModels/Transactions/Commands/UpdateTransactionStatusCommand.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Validators;
+using Domain.Enums;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Models.ApiModels.Transactions.Commands
+{
+    public class UpdateTransactionStatusCommand : IRequest
+    {
+        public string Id { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class UpdateTransactionStatusCommandValidator : AbstractValidator<UpdateTransactionStatusCommand>
+    {
+        public UpdateTransactionStatusCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().IdMustMatchGuidPattern();
+            RuleFor(x => x.Status).NotEmpty().IsEnumName(typeof(TransactionStatus));
+        }
+    }
+}
