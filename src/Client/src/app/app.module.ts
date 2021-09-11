4db77cfe9569036d18ddd0a1c6337430d1e1e694
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-09-11 15:46:17 +0200

    Add PageNotFound component

--- src/Client/src/app/app.module.ts
+++ src/Client/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {RouterModule} from "@angular/router";
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([
+      { path: '**', component: PageNotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
