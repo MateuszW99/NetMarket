cf9760983d8c9e18a75e6b8114e64f39a6b4d054
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-09-27 23:28:17 +0200

    Add Buy and SellNow command classes

---
+++ src/Application/Models/ApiModels/Transactions/Commands/SellNowCommand.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Validators;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Models.ApiModels.Transactions.Commands
+{
+    public class SellNowCommand : IRequest
+    {
+        public string BidId { get; set; }
+        public string SellerFee { get; set; }
+    }
+
+    public class SellNowCommandValidator : AbstractValidator<SellNowCommand>
+    {
+        public SellNowCommandValidator()
+        {
+            RuleFor(x => x.BidId).NotNull().IdMustMatchGuidPattern();
+            RuleFor(x => decimal.Parse(x.SellerFee)).GreaterThanOrEqualTo((decimal) 0.0);
+        }
+    }
+}
