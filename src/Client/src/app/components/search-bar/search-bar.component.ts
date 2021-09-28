e581c0ccba149cd50f209026206492921c6e63aa
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-09-29 00:05:48 +0200

    Make responsiveness better; add logout function

--- src/Client/src/app/components/search-bar/search-bar.component.ts
+++ src/Client/src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,12 +9,33 @@ import { FormGroup } from '@angular/forms';
 })
 export class SearchBarComponent implements OnInit {
   form: FormGroup;
+  dropdownIconPath: string;
+  isDropdownExpanded: boolean;
+  isUserLoggedIn: boolean;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({});
+
+    this.isDropdownExpanded = false;
+    this.dropdownIconPath = '../../../assets/expand_more.svg';
+    this.isUserLoggedIn = this.authService.isUserLoggedIn();
   }
 
   onSearch(): void {}
+
+  hideDropdown(): void {
+    this.isDropdownExpanded = false;
+    this.dropdownIconPath = '../../../assets/expand_more.svg';
+  }
+
+  showDropdown(): void {
+    this.isDropdownExpanded = true;
+    this.dropdownIconPath = '../../../assets/expand_less.svg';
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
