4aff16faf1f902351dc9130121070e10d57c2d01
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-10-15 17:24:34 +0200

    Add ISupervisorService and TransactionController

---
+++ src/Application/Handlers/TransactionHandlers/InitializeTransactionCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Models.ApiModels.Transactions.Commands;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Handlers.TransactionHandlers
+{
+    public class InitializeTransactionCommandHandler : IRequestHandler<InitializeTransactionCommand>
+    {
+        private readonly ITransactionService _transactionService;
+        private readonly ILogger<InitializeTransactionCommandHandler> _logger;
+        private readonly IAskService _askService;
+        private readonly IBidService _bidService;
+        private readonly ISupervisorService _supervisorService;
+
+        public InitializeTransactionCommandHandler(
+            ITransactionService transactionService,
+            ILogger<InitializeTransactionCommandHandler> logger, 
+            IAskService askService,
+            IBidService bidService, ISupervisorService supervisorService)
+        {
+            _transactionService = transactionService;
+            _logger = logger;
+            _askService = askService;
+            _bidService = bidService;
+            _supervisorService = supervisorService;
+        }
+
+        public async Task<Unit> Handle(InitializeTransactionCommand request, CancellationToken cancellationToken)
+        {
+            var ask = await _askService.GetAskByIdAsync(Guid.Parse(request.AskId));
+            var bid = await _bidService.GetBidByIdAsync(Guid.Parse(request.BidId));
+            var supervisorId = await _supervisorService.GetLeastLoadedSupervisorId();
+            await _transactionService.InitializeTransaction(ask, bid, DateTime.Now, supervisorId, cancellationToken);
+            _logger.LogInformation($"New transaction created for Ask {ask.Id} and Bid {bid.Id}");
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Application/Services/SupervisorService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Services
+{
+    public class SupervisorService : ISupervisorService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationDbContext _context;
+
+        public SupervisorService(UserManager<ApplicationUser> userManager, IApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+
+        public async Task<Guid> GetLeastLoadedSupervisorId()
+        {
+            var supervisors = await _userManager.GetUsersInRoleAsync(Roles.Supervisor);
+            var transactions = await _context.Transactions
+                .Where(x => 
+                    supervisors.Select(y => y.Id)
+                        .Contains(x.AssignedSupervisorId))
+                .GroupBy(x => (x.AssignedSupervisorId))
+                .Select(x => new
+                    {
+                        Id = x.Key, 
+                        Count = x.Count()
+                    })
+                .OrderBy(x => x.Count)
+                .ToListAsync();
+
+            var supervisorsWithoutTasks = supervisors.Where(x => 
+                    !transactions.Select(y => y.Id).Contains(x.Id))
+                .ToList();
+            
+            return supervisorsWithoutTasks.Any() ? supervisorsWithoutTasks.First().Id : transactions.First().Id;
+        }
+    }
+}
