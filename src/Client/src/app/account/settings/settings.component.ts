683b4ec600f96b96ef37274a840553de5a269f6d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-10-18 11:08:39 +0200

    Add subscriptions to settings component

--- src/Client/src/app/account/settings/settings.component.ts
+++ src/Client/src/app/account/settings/settings.component.ts
@@ -1,8 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/auth/user.model';
+import { SettingsService } from '../settings.service';
+import { UserSettings } from '../user-settings.model';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent {}
+export class SettingsComponent implements OnInit, OnDestroy {
+  user: User;
+  userSettings: UserSettings;
+  error = '';
+  loading = false;
+  userSubscription: Subscription;
+  userSettingsSubscription: Subscription;
+  errorSubscription: Subscription;
+  loadingSubscription: Subscription;
+
+  constructor(
+    private settingsService: SettingsService,
+    private authService: AuthService
+  ) {}
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe((user: User) => {
+      this.user = user;
+    });
+
+    this.settingsService.getUserSettings();
+
+    this.loadingSubscription = this.settingsService.loading.subscribe(
+      (isLoading) => {
+        this.loading = isLoading;
+      }
+    );
+
+    this.userSettingsSubscription =
+      this.settingsService.userSettingsChanged.subscribe(
+        (settings: UserSettings) => {
+          this.userSettings = settings;
+          this.error = '';
+        }
+      );
+
+    this.errorSubscription = this.settingsService.errorCatched.subscribe(
+      (errorMessage) => {
+        this.error = errorMessage;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+    this.userSettingsSubscription.unsubscribe();
+    this.errorSubscription.unsubscribe();
+    this.loadingSubscription.unsubscribe();
+  }
+}
