287e16a989fc2fbf3b1f9e89c27f0b633fbc33c3
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-10-25 11:18:24 +0200

    Add progress bar implementation

--- src/Client/src/app/account/profile/seller-level-progress/seller-level-progress.component.ts
+++ src/Client/src/app/account/profile/seller-level-progress/seller-level-progress.component.ts
@@ -10,5 +10,24 @@ export class SellerLevelProgressComponent {
   @Input() salesCompleted: number;
   @Input() sellerLevel: string;
   sellerLevels = SellerLevels.sellerLevels;
-  sellerLevelKeys = SellerLevels.sellerLevelKeys;
+  salesNeeded: Map<string, number> = new Map([
+    ['Beginner', 3],
+    ['Intermediate', 10],
+    ['Advanced', 25],
+    ['Business', 100]
+  ]);
+
+  getNeededSalesNumber(): number {
+    if (this.salesCompleted < 100) {
+      return this.salesNeeded.get(this.sellerLevel) - this.salesCompleted;
+    }
+
+    return 0;
+  }
+
+  getProgressBarWidth(): string {
+    const width =
+      (this.salesCompleted / this.salesNeeded.get(this.sellerLevel)) * 100;
+    return width.toString() + '%';
+  }
 }
