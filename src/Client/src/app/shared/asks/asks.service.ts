331b5fae73fa170d68c059e27283db2fec66ae97
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-10-27 22:57:41 +0200

    Implement placeAsk method

--- src/Client/src/app/shared/asks/asks.service.ts
+++ src/Client/src/app/shared/asks/asks.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { ApiPaths } from "../api-paths";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsksService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  placeAsk(itemId: string, size: string, price: string): Observable<any> {
+
+    return this.http.post(
+      environment.apiUrl + ApiPaths.Asks,
+      { itemId, size, price });
+  }
 }
