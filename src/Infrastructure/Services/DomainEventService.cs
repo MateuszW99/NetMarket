69fd0ee7c15ced3e0855ce4a34a99fc18d0dc819
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-11-02 20:56:27 +0100

    Implement DomainEventService

--- src/Infrastructure/Services/DomainEventService.cs
+++ src/Infrastructure/Services/DomainEventService.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Application.Common.Events;
 using Application.Common.Interfaces;
 using Domain.Common;
+using MediatR;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Services
@@ -8,16 +11,25 @@ namespace Infrastructure.Services
     public class DomainEventService : IDomainEventService
     {
         private readonly ILogger<DomainEventService> _logger;
+        private readonly IMediator _mediator;
         
-        public DomainEventService(ILogger<DomainEventService> logger)
+        public DomainEventService(ILogger<DomainEventService> logger, IMediator mediator)
         {
             _logger = logger;
+            _mediator = mediator;
         }
         
-        public Task Publish(DomainEvent domainEvent)
+        public async Task Publish(DomainEvent domainEvent)
         {
-            _logger.LogInformation($"Publishing {domainEvent.GetType().Namespace} event.");
-            return Task.CompletedTask; // TODO: add mediator publish method
+            _logger.LogInformation($"Publishing {domainEvent.GetType()} event.");
+            await _mediator.Publish(GetNotificationCorrespondingToDomainEvent(domainEvent));
+        }
+
+        private INotification GetNotificationCorrespondingToDomainEvent(DomainEvent domainEvent)
+        {
+            return (INotification)Activator.CreateInstance(
+                typeof(DomainEventNotification<>)
+                    .MakeGenericType(domainEvent.GetType()), domainEvent);
         }
     }
 }
