23cb4b77d40c9cb5fd7983d844a8633d8509d17f
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-11-02 20:56:09 +0100

    Add TransactionInitializedEvent

---
+++ src/Domain/Events/TransactionInitializedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Domain.Common;
+
+namespace Domain.Events
+{
+    public class TransactionInitializedEvent : DomainEvent
+    {
+        public TransactionInitializedEvent(Guid sellerId)
+        {
+            SellerId = sellerId;
+        }
+        
+        public Guid SellerId { get; }
+    }
+}
