e77ea38bcd76b6d9e6950dd118c71fa810f0a9fa
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-11-03 22:58:16 +0100

    Add sellNow and buyNow methods

--- src/Client/src/app/shared/services/orders/transaction.service.ts
+++ src/Client/src/app/shared/services/orders/transaction.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../../../environments/environment";
+import { ApiPaths } from "../../api-paths";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sellNow(bidId: string): Observable<unknown> {
+    const askId = '';
+    return this.http.post(environment.apiUrl + ApiPaths.Transactions, {
+      askId,
+      bidId
+    });
+  }
+
+  buyNow(askId: string): Observable<unknown> {
+    const bidId = '';
+    return this.http.post(environment.apiUrl + ApiPaths.Transactions, {
+      askId,
+      bidId
+    });
+  }
 }
