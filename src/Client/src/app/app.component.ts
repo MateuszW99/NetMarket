0a57f73e4e128ce2269bc822a4fc96b45dea446f
Mateusz Wiczuk <mwiczuk99@gmail.com>
2021-11-03 22:58:03 +0100

    Configure toastr to remove duplicates

--- src/Client/src/app/app.component.ts
+++ src/Client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, DoCheck, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { RoutingService } from './shared/services/routing/routing.service';
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,13 @@ export class AppComponent implements OnInit, DoCheck {
   public isAuthPage: boolean;
   constructor(
     private authService: AuthService,
-    private routingService: RoutingService
+    private routingService: RoutingService,
+    private toastrService: ToastrService
   ) {}
 
   ngOnInit(): void {
     this.authService.autoLogin();
+    this.toastrService.toastrConfig.preventDuplicates = true;
   }
 
   ngDoCheck(): void {
