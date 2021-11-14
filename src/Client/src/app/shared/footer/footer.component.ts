a72de54912e372d2bfac52ee5cef900374e20718
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-11-14 14:08:17 +0100

    Change account icon redirect page based on role

--- src/Client/src/app/shared/footer/footer.component.ts
+++ src/Client/src/app/shared/footer/footer.component.ts
@@ -1,31 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AuthService } from 'src/app/auth/auth.service';
-import { User } from 'src/app/auth/user.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit, OnDestroy {
-  role: string;
-  userSubscription: Subscription
-  constructor(private authService: AuthService) { }
-
-  ngOnInit(): void {
-    this.userSubscription = this.authService.user.subscribe((user: User) => {
-      if(user != null){
-        this.role = user.role;
-      }
-      else{
-        this.role === 'notLoggedIn';
-      }
-    }) 
-  }
-
-  ngOnDestroy(): void {
-    this.userSubscription.unsubscribe();
-  }
-
-}
+export class FooterComponent {}
