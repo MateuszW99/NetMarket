25625913fbb17a88d133293513dec432a743ddff
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-11-14 15:28:20 +0100

    Add GetSupervisors, AddSupervisor and DeleteSupervisor endpoints

--- src/Api/Controllers/AdminPanelController.cs
+++ src/Api/Controllers/AdminPanelController.cs
@@ -1,8 +1,13 @@
 ﻿using System.Threading.Tasks;
+using Application.Common.Interfaces;
 using Application.Common.Models;
+using Application.Identity.Requests;
+using Application.Identity.Responses;
+using Application.Models.ApiModels.Supervisors.Queries;
 using Application.Models.ApiModels.Transactions.Commands;
 using Application.Models.ApiModels.Transactions.Queries;
 using Application.Models.DTOs;
+using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,10 +21,12 @@ namespace Api.Controllers
     public class AdminPanelController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly IIdentityService _identityService;
 
-        public AdminPanelController(IMediator mediator)
+        public AdminPanelController(IMediator mediator, IIdentityService identityService)
         {
             _mediator = mediator;
+            _identityService = identityService;
         }
 
         [HttpGet("orders")]
@@ -40,6 +47,49 @@ namespace Api.Controllers
         {
             await _mediator.Send(command);
             return Ok();
+        }
+        
+        [HttpGet("supervisors")]
+        public async Task<IActionResult> GetSupervisors([FromQuery]GetSupervisorsQuery query)
+        {
+            
+            var result = await _mediator.Send(new GetSupervisorsQuery()
+            {
+                PageIndex = query.PageIndex,
+                PageSize = query.PageSize,
+                SearchQuery = query.SearchQuery
+            });
+
+            return Ok(result);
+        }
+        
+        [HttpPost("supervisors")]
+        public async Task<IActionResult> AddSupervisor([FromBody]UserRegistrationRequest request)
+        {
+            var registrationResponse = await _identityService.RegisterAsync(request.Email, request.UserName, request.Password, Roles.Supervisor);
+
+            if (!registrationResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse()
+                {
+                    ErrorMessages = registrationResponse.ErrorMessages
+                });
+            }
+            
+            return Ok();
+        }
+        
+        [HttpDelete("supervisors/{id}")]
+        public async Task<IActionResult> DeleteSupervisor(string id)
+        {
+            var deleteResponse = await _identityService.DeleteUserAsync(id);
+
+            if (!deleteResponse.Success)
+            {
+                return BadRequest(deleteResponse);
+            }
+            
+            return Ok(deleteResponse);
         }
     }
 }
--- src/Application/Services/SupervisorService.cs
+++ src/Application/Services/SupervisorService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Common.Interfaces;
+using Application.Models.DTOs;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application.Services
@@ -10,11 +12,13 @@ namespace Application.Services
     {
         private readonly IApplicationDbContext _context;
         private readonly IUserManagerService _userManagerService;
+        private readonly IIdentityService _identityService;
 
-        public SupervisorService(IApplicationDbContext context, IUserManagerService userManagerService)
+        public SupervisorService(IApplicationDbContext context, IUserManagerService userManagerService, IIdentityService identityService)
         {
             _context = context;
             _userManagerService = userManagerService;
+            _identityService = identityService;
         }
         
         public async Task<Guid> GetLeastLoadedSupervisorId(string role)
@@ -35,6 +39,19 @@ namespace Application.Services
                 .ToList();
             
             return supervisorsWithoutTasks.Any() ? supervisorsWithoutTasks.First() : transactions.First().Id;
+        }
+        
+        public async Task<List<SupervisorObject>> GetSupervisorsAsync(string searchQuery)
+        {
+            var supervisors = await _identityService.GetSupervisorsAsync();
+            var queriedSupervisors = supervisors.AsQueryable();
+            
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                queriedSupervisors = queriedSupervisors.Where(x => x.Email.ToLower().Contains(searchQuery.ToLower()));
+            }
+
+            return queriedSupervisors.ToList();
         }
     }
 }
